4d811d5cbfbc94464ca522d245c6001342ffd621
jhuva <jdzambranoh@unac.edu.pe>
2021-10-03 23:19:20 -0500

    Agregando los lazyload, modularizando la ap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
+import { AuthRoutingModule } from './auth/auth.routing';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+import { PagesRoutingModule } from './pages/pages.routing';
 
 const routes: Routes = [
+
   {
     path: '',
-    component: PagesComponent,
-    children: [
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'progress',
-        component: ProgressComponent
-      },
-      {
-        path: 'grafica1',
-        component: Grafica1Component
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard', pathMatch: 'full'
-      },
-    ]
+    redirectTo: '/dashboard', pathMatch: 'full'
   },
-
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
-  },
-
   {
     path: '**',
     component: NotpagefoundComponent
@@ -50,7 +21,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [
     RouterModule
