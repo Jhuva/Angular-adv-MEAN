d8794ced40a98ad7c0a2240f0563ecaa5771e3cb
jhuva <jdzambranoh@unac.edu.pe>
2021-10-27 22:42:37 -0500

    Agregando nuevas graficas, componente incrementador y graficas donas

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  public labels1: string[] = ['Jarabes', 'Pastillas', 'Vacunas'];
+  public data1 = [
+    [50, 40, 70]
+  ];
 
 }
