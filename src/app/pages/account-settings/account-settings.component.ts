64d08ddb5124cf429bdf8e5f0a7ab8645dcaea68
jhuva <jdzambranoh@unac.edu.pe>
2021-11-01 17:24:29 -0500

    Agregando el check cuando se cambie los temas

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingsComponent implements OnInit {
 
   public linktheme = document.querySelector('#theme');
+  public links!: NodeListOf<Element>; 
   constructor() { }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme()
   }
 
   changeTheme( theme: string ) {
@@ -18,6 +21,23 @@ export class AccountSettingsComponent implements OnInit {
     
     this.linktheme?.setAttribute('href', url);
     localStorage.setItem('theme', url)
+    
+    this.checkCurrentTheme();
+    
+  }
+
+  checkCurrentTheme() {
+    this.links.forEach( elem => {
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
+      const currentTheme = this.linktheme?.getAttribute('href');
+
+      if ( btnThemeUrl === currentTheme ) {
+        elem.classList.add('working');
+      }
+
+    } )
   }
 
 }
