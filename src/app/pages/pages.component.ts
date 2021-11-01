013a103a014d6cddc358b8cece606c73227a4c45
jhuva <jdzambranoh@unac.edu.pe>
2021-11-01 17:38:25 -0500

    Optimizando mi codigo, he creado un servicio setting.service para modificar los colores de los temas

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../services/settings.service';
 
 @Component({
   selector: 'app-pages',
@@ -8,14 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  public linktheme = document.querySelector('#theme');
 
-  constructor() { }
+  constructor( private settingsService: SettingsService ) { }
 
   ngOnInit(): void {
-
-    const url = localStorage.getItem('theme') || './assets/css/colors/default-dark.css'
-    this.linktheme?.setAttribute('href', url);
 
   }
 
