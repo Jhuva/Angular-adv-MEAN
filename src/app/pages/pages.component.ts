8f87e645026c5df51cb17e35ca443a42241e050b
jhuva <jdzambranoh@unac.edu.pe>
2021-11-01 18:29:45 -0500

    Usando los javascript del tema y declarandolo en el componente

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
+//@ts-ignore
+declare function customInitFunctions();
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -13,7 +16,7 @@ export class PagesComponent implements OnInit {
   constructor( private settingsService: SettingsService ) { }
 
   ngOnInit(): void {
-
+    customInitFunctions();
   }
 
 }
